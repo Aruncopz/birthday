415258b3b44b0545d1d123cc026ed0f3d23e6916
Arun <arunjothi2599@gmail.com>
2024-06-28 22:18:47 +0530

    initial commit

---
+++ home/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+export const routes: Routes = [
+    {path:'content', component:ContentComponent},
+];
